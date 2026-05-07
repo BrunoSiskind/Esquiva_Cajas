16c51c5bbd8fb938ada9f299c7bdc8dcf73fe329
BrunoSiskind <Brunosiskind10@gmail.com>
2026-05-07 10:00:46 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetkeyDown(KeyCode.D))
+        transform.Translate(2,0,0);
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetkeyDown(KeyCode.A))
+        transform.Translate(-2,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
