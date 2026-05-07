0515490ed6930f24c40899e84002b7a6270eb751
BrunoSiskind <Brunosiskind10@gmail.com>
2026-05-07 12:04:12 -0300

    doneit

--- Assets/Scripts/BadCajaMov.cs
+++ Assets/Scripts/BadCajaMov.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BadCajaMov : MonoBehaviour
-{
+{   
     public float velocidad = 3f;
+     float InitialYValue;
     // Start is called before the first frame update
     void Start()
     {
         
+        InitialYValue = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down*velocidad*Time.deltaTime);
+    }
+
+    public void Moveboxtorandomposition(){
+        Vector3 newPosition = transform.position;
+        int RandomX = 0;
+        RandomX = Random.Range(0,3);
+        if (RandomX == 0){
+            newPosition.x = -2;
+        }
+        else if (RandomX == 1){
+            newPosition.x = 0;
+        }
+        else{
+            newPosition.x = 2;
+        }
+        
+        newPosition.y = InitialYValue;
+        transform.position = newPosition;
     }
 }
---
+++ Assets/Scripts/Cajacollisiondetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cajacollisiondetection : MonoBehaviour
+{
+    public BadCajaMov cajamovementScript;
+    float InitialYValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cajamovementScript = GetComponent<BadCajaMov>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Piso"))
+        {
+            cajamovementScript.Moveboxtorandomposition();
+        }
+    }
+}
