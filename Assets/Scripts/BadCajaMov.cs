ffab55989d72d381d46d5610de82cdd769ede6e3
BrunoSiskind <Brunosiskind10@gmail.com>
2026-05-07 12:08:03 -0300

    Döner Kabab

--- Assets/Scripts/BadCajaMov.cs
+++ Assets/Scripts/BadCajaMov.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BadCajaMov : MonoBehaviour
 {   
-    public float velocidad = 3f;
+    public float velocidad = 8f;
      float InitialYValue;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,7 @@ public class BadCajaMov : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down*velocidad*Time.deltaTime);
+        transform.Translate(0,-velocidad*Time.deltaTime,0);
     }
 
     public void Moveboxtorandomposition(){
