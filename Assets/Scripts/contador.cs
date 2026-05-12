69c4611d195fcf9d85dbbcb9f1edace219e8ee81
BrunoSiskind <Brunosiskind10@gmail.com>
2026-05-12 14:28:23 -0300

    tiempo

---
+++ Assets/Scripts/contador.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class contador : MonoBehaviour
+{
+    public TextMeshProUGUI Contador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Contador.text = Time.time.ToString("F2");
+    }
+}
